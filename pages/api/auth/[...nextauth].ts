4f0ce06f0e71dac9d43d0f55d673070c2768e9a8
Andrés Martínez <andresmarpz@gmail.com>
2023-01-21 22:34:04 -0300

    install prisma and set up with next-auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,11 @@
+import client from '@/prisma/client'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import NextAuth, { NextAuthOptions } from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 
 export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
-  jwt: {
-    maxAge: 30 * 24 * 60 * 60 // 30 days
-  },
+  adapter: PrismaAdapter(client),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
