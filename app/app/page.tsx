b2976d7780c558bd9074d0b2a361cfea5f08ce19
Andrés Martínez <andresmarpz@gmail.com>
2023-01-21 23:01:11 -0300

    basic prisma data fetching

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,17 +1,34 @@
-import ClientTest from '@/components/ClientTest'
+import { authOptions } from '@/pages/api/auth/[...nextauth]'
 import { unstable_getServerSession } from 'next-auth/next'
-import { redirect } from 'next/navigation';
+import { redirect } from 'next/navigation'
 
 export default async function App() {
-  const session = await unstable_getServerSession()
+  const session = await unstable_getServerSession(authOptions)
 
-  if (!session) 
-		redirect('/api/auth/signin');
+  if (!session) redirect('/api/auth/signin')
+
+  const bookmarks = await prisma?.bookmark.findMany({
+    where: {
+      user: {
+        email: session!.user?.email
+      }
+    }
+  })
 
   return (
-    <div>
-      Hello, from the server! {session!.user?.name}
-      <ClientTest />
-    </div>
+    <>
+      <div>Hello, from the server! {session!.user?.name}</div>
+      <div>
+        Bookmarks:
+        <ul>
+          {bookmarks?.map((bookmark) => (
+            <li key={bookmark.id}>
+              {bookmark.title}
+              {bookmark.url}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   )
 }
