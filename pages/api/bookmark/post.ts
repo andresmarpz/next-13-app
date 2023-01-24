c1ca7358c61801c5a97bb3f665619fbb6d608d16
Andrés Martínez <andresmarpz@gmail.com>
2023-01-23 23:53:48 -0300

    add endpoints to create collections and bookmarks

---
+++ pages/api/bookmark/post.ts
@@ -0,0 +1,56 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { unstable_getServerSession } from 'next-auth'
+import { authOptions } from '../auth/[...nextauth]'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const session = await unstable_getServerSession(req, res, authOptions)
+
+  if (!session) return res.status(401).json({ error: 'Unauthorized' })
+
+  try {
+    const { title, description, url, collections } = JSON.parse(req.body)
+
+    const userCollections = await prisma?.collection.findMany({
+      where: {
+        user: {
+          email: session!.user?.email!
+        }
+      }
+    })
+
+    // if collections (list of collection ids) contains a collection id that is not in userCollections, throw error
+    if (
+      collections.some(
+        (collection: number) =>
+          !userCollections?.some(
+            (userCollection) => userCollection.id === collection
+          )
+      )
+    ) {
+      return res.status(401).json({
+        error: 'Can not add bookmark to collection that does not belong to user'
+      })
+    }
+
+    const bookmark = await prisma?.bookmark.create({
+      data: {
+        title,
+        description,
+        url,
+        image: 'asdasdass',
+        collections
+      }
+    })
+
+    res.status(200).json({
+      bookmark
+    })
+  } catch (error) {
+    res.status(400).json({
+      error
+    })
+  }
+}
---
+++ pages/api/collection/post.ts
@@ -0,0 +1,36 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { unstable_getServerSession } from 'next-auth'
+import { authOptions } from '../auth/[...nextauth]'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'POST') {
+    const session = await unstable_getServerSession(req, res, authOptions)
+
+    if (!session) {
+      return res.status(401).json({ error: 'Unauthorized' })
+    }
+
+    try {
+      const { name } = JSON.parse(req.body)
+
+      const collection = await prisma?.collection.create({
+        data: {
+          name,
+          user: {
+            connect: {
+              email: session!.user?.email!
+            }
+          }
+        }
+      })
+      console.log(collection)
+
+      return res.status(200).json({ collection })
+    } catch (error) {
+      return res.status(400).json({ error: 'Bad request' })
+    }
+  }
+}
