0fa0eb0b1c331dde7cbdd3b28a549bedc8b72499
Andrés Martínez <andresmarpz@gmail.com>
2023-01-23 23:51:58 -0300

    create basic useAsync hook

---
+++ hooks/use-async.ts
@@ -0,0 +1,41 @@
+import { useRouter } from 'next/navigation'
+import { useCallback, useEffect, useState, useTransition } from 'react'
+
+export default function useAsync<Data = void>(
+  callback: () => Promise<Data>,
+  { refresh = false, immediate = false } = {}
+) {
+  const router = useRouter()
+  const [isPending, startTransition] = useTransition()
+  const [data, setData] = useState<Data | undefined>(undefined)
+  const [error, setError] = useState(undefined)
+  const [isFetching, setIsFetching] = useState(false)
+
+  const isLoading = isFetching || isPending
+
+  const execute = useCallback(async () => {
+    setError(undefined)
+    setData(undefined)
+    setIsFetching(true)
+    const response = await callback()
+      .then((res) => setData(res))
+      .catch((err) => setError(err))
+
+    setIsFetching(false)
+
+    if (refresh) startTransition(() => router.refresh())
+
+    return response
+  }, [callback, refresh, router])
+
+  useEffect(() => {
+    if (immediate) execute()
+  }, [execute, immediate])
+
+  return {
+    data,
+    isLoading,
+    hasError: error,
+    execute
+  }
+}
